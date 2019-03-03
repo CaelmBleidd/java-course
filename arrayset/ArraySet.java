1265fe742c3afe52f5bb663511899fc59adc2466
CaelmBleidd <alex.menshutin99@gmail.com>
2019-03-03 18:20:23 +0300

    arrayset done

---
+++ arrayset/ArraySet.java
@@ -0,0 +1,420 @@
+package ru.ifmo.rain.menshutin.arrayset;
+
+import java.util.*;
+
+public class ArraySet<E extends Comparable> extends AbstractSet<E> implements NavigableSet<E> {
+    private List<E> elements;
+    private Comparator<? super E> comparator;
+
+    public ArraySet() {
+        elements = Collections.emptyList();
+        comparator = null;
+    }
+
+    public ArraySet(Collection<? extends E> other) {
+        elements = new ArrayList<>(new TreeSet<>(other));
+        comparator = null;
+    }
+
+    public ArraySet(Collection<? extends E> other, Comparator<? super E> comparator) {
+        var tmp = new TreeSet<E>(comparator);
+        tmp.addAll(other);
+        elements = new ArrayList<>(tmp);
+        this.comparator = comparator;
+    }
+
+
+    public ArraySet(List<E> elements, Comparator<? super E> comparator) {
+        this.elements = elements;
+        this.comparator = comparator;
+    }
+
+
+    /**
+     * Returns the greatest element in this set strictly less than the
+     * given element, or {@code null} if there is no such element.
+     *
+     * @param e the value to match
+     * @return the greatest element less than {@code e},
+     * or {@code null} if there is no such element
+     * @throws ClassCastException   if the specified element cannot be
+     *                              compared with the elements currently in the set
+     * @throws NullPointerException if the specified element is null
+     *                              and this set does not permit null elements
+     */
+    @Override
+    public E lower(E e) {
+        int index = lowerIndex(e);
+        return getElementOrNull(index);
+    }
+
+    private int lowerIndex(E e) {
+        int index = binarySearch(e);
+        return index >= 0 ? index - 1 : (-index - 1) - 1;
+    }
+
+    /**
+     * Returns the greatest element in this set less than or equal to
+     * the given element, or {@code null} if there is no such element.
+     *
+     * @param e the value to match
+     * @return the greatest element less than or equal to {@code e},
+     * or {@code null} if there is no such element
+     * @throws ClassCastException   if the specified element cannot be
+     *                              compared with the elements currently in the set
+     * @throws NullPointerException if the specified element is null
+     *                              and this set does not permit null elements
+     */
+    @Override
+    public E floor(E e) {
+        int index = floorIndex(e);
+        return getElementOrNull(index);
+    }
+
+    private int floorIndex(E e) {
+        int index = binarySearch(e);
+        return index >= 0 ? index : (-index - 1) - 1;
+
+    }
+
+    /**
+     * Returns the least element in this set greater than or equal to
+     * the given element, or {@code null} if there is no such element.
+     *
+     * @param e the value to match
+     * @return the least element greater than or equal to {@code e},
+     * or {@code null} if there is no such element
+     * @throws ClassCastException   if the specified element cannot be
+     *                              compared with the elements currently in the set
+     * @throws NullPointerException if the specified element is null
+     *                              and this set does not permit null elements
+     */
+    @Override
+    public E ceiling(E e) {
+        int index = ceilingIndex(e);
+        return getElementOrNull(index);
+    }
+
+    private int ceilingIndex(E e) {
+        int index = binarySearch(e);
+        return index >= 0 ? index : (-index - 1);
+    }
+
+    /**
+     * Returns the least element in this set strictly greater than the
+     * given element, or {@code null} if there is no such element.
+     *
+     * @param e the value to match
+     * @return the least element greater than {@code e},
+     * or {@code null} if there is no such element
+     * @throws ClassCastException   if the specified element cannot be
+     *                              compared with the elements currently in the set
+     * @throws NullPointerException if the specified element is null
+     *                              and this set does not permit null elements
+     */
+    @Override
+    public E higher(E e) {
+        int index = higherIndex(e);
+        return getElementOrNull(index);
+    }
+
+    private int higherIndex(E e) {
+        int index = binarySearch(e);
+        return index >= 0 ? index + 1 : (-index - 1);
+    }
+
+    /**
+     * Retrieves and removes the first (lowest) element,
+     * or returns {@code null} if this set is empty.
+     *
+     * @return the first element, or {@code null} if this set is empty
+     */
+    @Override
+    public E pollFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Retrieves and removes the last (highest) element,
+     * or returns {@code null} if this set is empty.
+     *
+     * @return the last element, or {@code null} if this set is empty
+     */
+    @Override
+    public E pollLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns an iterator over the elements contained in this collection.
+     *
+     * @return an iterator over the elements contained in this collection
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return Collections.unmodifiableCollection(elements).iterator();
+    }
+
+    /**
+     * Returns a reverse order view of the elements contained in this set.
+     * The descending set is backed by this set, so changes to the set are
+     * reflected in the descending set, and vice-versa.  If either set is
+     * modified while an iteration over either set is in progress (except
+     * through the iterator's own {@code remove} operation), the results of
+     * the iteration are undefined.
+     *
+     * <p>The returned set has an ordering equivalent to
+     * {@link Collections#reverseOrder(Comparator) Collections.reverseOrder}{@code (comparator())}.
+     * The expression {@code s.descendingSet().descendingSet()} returns a
+     * view of {@code s} essentially equivalent to {@code s}.
+     *
+     * @return a reverse order view of this set
+     */
+    @Override
+    public NavigableSet<E> descendingSet() {
+        return new ArraySet<>(new ReverseListView<>(elements), Collections.reverseOrder(comparator));
+    }
+
+    /**
+     * Returns an iterator over the elements in this set, in descending order.
+     * Equivalent in effect to {@code descendingSet().iterator()}.
+     *
+     * @return an iterator over the elements in this set, in descending order
+     */
+    @Override
+    public Iterator<E> descendingIterator() {
+        return descendingSet().iterator();
+    }
+
+    /**
+     * Returns a view of the portion of this set whose elements range from
+     * {@code fromElement} to {@code toElement}.  If {@code fromElement} and
+     * {@code toElement} are equal, the returned set is empty unless {@code
+     * fromInclusive} and {@code toInclusive} are both true.  The returned set
+     * is backed by this set, so changes in the returned set are reflected in
+     * this set, and vice-versa.  The returned set supports all optional set
+     * operations that this set supports.
+     *
+     * <p>The returned set will throw an {@code IllegalArgumentException}
+     * on an attempt to insert an element outside its range.
+     *
+     * @param fromElement   low endpoint of the returned set
+     * @param fromInclusive {@code true} if the low endpoint
+     *                      is to be included in the returned view
+     * @param toElement     high endpoint of the returned set
+     * @param toInclusive   {@code true} if the high endpoint
+     *                      is to be included in the returned view
+     * @return a view of the portion of this set whose elements range from
+     * {@code fromElement}, inclusive, to {@code toElement}, exclusive
+     * @throws ClassCastException       if {@code fromElement} and
+     *                                  {@code toElement} cannot be compared to one another using this
+     *                                  set's comparator (or, if the set has no comparator, using
+     *                                  natural ordering).  Implementations may, but are not required
+     *                                  to, throw this exception if {@code fromElement} or
+     *                                  {@code toElement} cannot be compared to elements currently in
+     *                                  the set.
+     * @throws NullPointerException     if {@code fromElement} or
+     *                                  {@code toElement} is null and this set does
+     *                                  not permit null elements
+     * @throws IllegalArgumentException if {@code fromElement} is
+     *                                  greater than {@code toElement}; or if this set itself
+     *                                  has a restricted range, and {@code fromElement} or
+     *                                  {@code toElement} lies outside the bounds of the range.
+     */
+    @Override
+    public NavigableSet<E> subSet(E fromElement, boolean fromInclusive, E toElement, boolean toInclusive) {
+
+        if (compare(fromElement, toElement) > 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int fromIndex = fromInclusive ? ceilingIndex(fromElement) : higherIndex(fromElement);
+        int toIndex = toInclusive ? floorIndex(toElement) : lowerIndex(toElement);
+
+        if (toIndex + 2 == fromIndex)
+            toIndex = fromIndex - 1;
+
+
+        return new ArraySet<>(elements.subList(fromIndex, toIndex + 1), comparator);
+    }
+
+    private int compare(E first, E second) {
+        if (comparator != null) {
+            return comparator.compare(first, second);
+        }
+        List<E> orderedList = Arrays.asList(first, second);
+        orderedList.sort(null);
+        return orderedList.get(0) == first ? -1 : 1;
+    }
+
+    /**
+     * Returns a view of the portion of this set whose elements are less than
+     * (or equal to, if {@code inclusive} is true) {@code toElement}.  The
+     * returned set is backed by this set, so changes in the returned set are
+     * reflected in this set, and vice-versa.  The returned set supports all
+     * optional set operations that this set supports.
+     *
+     * <p>The returned set will throw an {@code IllegalArgumentException}
+     * on an attempt to insert an element outside its range.
+     *
+     * @param toElement high endpoint of the returned set
+     * @param inclusive {@code true} if the high endpoint
+     *                  is to be included in the returned view
+     * @return a view of the portion of this set whose elements are less than
+     * (or equal to, if {@code inclusive} is true) {@code toElement}
+     * @throws ClassCastException       if {@code toElement} is not compatible
+     *                                  with this set's comparator (or, if the set has no comparator,
+     *                                  if {@code toElement} does not implement {@link Comparable}).
+     *                                  Implementations may, but are not required to, throw this
+     *                                  exception if {@code toElement} cannot be compared to elements
+     *                                  currently in the set.
+     * @throws NullPointerException     if {@code toElement} is null and
+     *                                  this set does not permit null elements
+     * @throws IllegalArgumentException if this set itself has a
+     *                                  restricted range, and {@code toElement} lies outside the
+     *                                  bounds of the range
+     */
+    @Override
+    public NavigableSet<E> headSet(E toElement, boolean inclusive) {
+        int index = inclusive ? floorIndex(toElement) : lowerIndex(toElement);
+        return new ArraySet<>(elements.subList(0, index + 1), comparator);
+    }
+
+    /**
+     * Returns a view of the portion of this set whose elements are greater
+     * than (or equal to, if {@code inclusive} is true) {@code fromElement}.
+     * The returned set is backed by this set, so changes in the returned set
+     * are reflected in this set, and vice-versa.  The returned set supports
+     * all optional set operations that this set supports.
+     *
+     * <p>The returned set will throw an {@code IllegalArgumentException}
+     * on an attempt to insert an element outside its range.
+     *
+     * @param fromElement low endpoint of the returned set
+     * @param inclusive   {@code true} if the low endpoint
+     *                    is to be included in the returned view
+     * @return a view of the portion of this set whose elements are greater
+     * than or equal to {@code fromElement}
+     * @throws ClassCastException       if {@code fromElement} is not compatible
+     *                                  with this set's comparator (or, if the set has no comparator,
+     *                                  if {@code fromElement} does not implement {@link Comparable}).
+     *                                  Implementations may, but are not required to, throw this
+     *                                  exception if {@code fromElement} cannot be compared to elements
+     *                                  currently in the set.
+     * @throws NullPointerException     if {@code fromElement} is null
+     *                                  and this set does not permit null elements
+     * @throws IllegalArgumentException if this set itself has a
+     *                                  restricted range, and {@code fromElement} lies outside the
+     *                                  bounds of the range
+     */
+    @Override
+    public NavigableSet<E> tailSet(E fromElement, boolean inclusive) {
+        int index = inclusive ? ceilingIndex(fromElement) : higherIndex(fromElement);
+        return new ArraySet<>(elements.subList(index, size()), comparator);
+    }
+
+    /**
+     * Returns the comparator used to order the elements in this set,
+     * or {@code null} if this set uses the {@linkplain Comparable
+     * natural ordering} of its elements.
+     *
+     * @return the comparator used to order the elements in this set,
+     * or {@code null} if this set uses the natural ordering
+     * of its elements
+     */
+    @Override
+    public Comparator<? super E> comparator() {
+        return comparator;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>Equivalent to {@code subSet(fromElement, true, toElement, false)}.
+     *
+     * @param fromElement
+     * @param toElement
+     * @throws ClassCastException       {@inheritDoc}
+     * @throws NullPointerException     {@inheritDoc}
+     * @throws IllegalArgumentException {@inheritDoc}
+     */
+    @Override
+    public SortedSet<E> subSet(E fromElement, E toElement) {
+        return subSet(fromElement, true, toElement, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>Equivalent to {@code headSet(toElement, false)}.
+     *
+     * @param toElement
+     * @throws ClassCastException       {@inheritDoc}
+     * @throws NullPointerException     {@inheritDoc}
+     * @throws IllegalArgumentException {@inheritDoc}
+     */
+    @Override
+    public SortedSet<E> headSet(E toElement) {
+        return headSet(toElement, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>Equivalent to {@code tailSet(fromElement, true)}.
+     *
+     * @param fromElement
+     * @throws ClassCastException       {@inheritDoc}
+     * @throws NullPointerException     {@inheritDoc}
+     * @throws IllegalArgumentException {@inheritDoc}
+     */
+    @Override
+    public SortedSet<E> tailSet(E fromElement) {
+        return tailSet(fromElement, true);
+    }
+
+    /**
+     * Returns the first (lowest) element currently in this set.
+     *
+     * @return the first (lowest) element currently in this set
+     * @throws NoSuchElementException if this set is empty
+     */
+    @Override
+    public E first() {
+        if (elements.isEmpty())
+            throw new NoSuchElementException();
+        return elements.get(0);
+    }
+
+    /**
+     * Returns the last (highest) element currently in this set.
+     *
+     * @return the last (highest) element currently in this set
+     * @throws NoSuchElementException if this set is empty
+     */
+    @Override
+    public E last() {
+        if (elements.isEmpty())
+            throw new NoSuchElementException();
+        return elements.get(size() - 1);
+    }
+
+    @Override
+    public int size() {
+        return elements.size();
+    }
+
+    private int binarySearch(E e) {
+        return (Collections.binarySearch(elements, Objects.requireNonNull(e), comparator));
+    }
+
+    private E getElementOrNull(int index) {
+        return (index < 0 || index >= size()) ? null : elements.get(index);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean contains(Object o) {
+        return (binarySearch((E) o) >= 0);
+    }
+}
